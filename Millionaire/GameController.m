96cea55d19dcc112da764fea7bb58450c07616d3
Maksim Romanov <ios38ru@gmail.com>
2020-05-09 20:14:27 +0800

    Set up fifty-fifty feature

--- Millionaire/GameController.m
+++ Millionaire/GameController.m
@@ -24,6 +24,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *questionDifficulty;
 @property (weak, nonatomic) IBOutlet UILabel *QuestionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *trueAnswersCountLabel;
+@property (weak, nonatomic) IBOutlet UIButton *fiftyFiftyButton;
 
 @property (assign,nonatomic)int questionsType;
 @property (strong,nonatomic)QuestionAndAnswers *questionAndAnswers;
@@ -38,8 +39,11 @@
     [super viewDidLoad];
     self.questionsType = 1;
     self.questionAndAnswers = [[QuestionAndAnswers alloc] init];
+    self.QuestionLabel.text = self.questionAndAnswers.question;
     self.questionDifficulty.text = @"Уровень сложности: 1";
     self.trueAnswersCountLabel.text = @"Правильных ответов: 0";
+    
+    [self.fiftyFiftyButton addTarget:self action:@selector(fiftyFiftyAnswers) forControlEvents:UIControlEventTouchUpInside];
     [self getQuestion];
 }
 
@@ -106,6 +110,8 @@
     }
 }
 
+#pragma mark - Setters
+
 - (void)setTrueAnswersCount:(NSUInteger)trueAnswersCount {
     _trueAnswersCount = trueAnswersCount;
     self.trueAnswersCountLabel.text = [NSString stringWithFormat:@"Правильных ответов: %lu", (unsigned long)self.trueAnswersCount];
@@ -114,15 +120,24 @@
 - (void)setQuestionsType:(int)questionsType {
     _questionsType = questionsType;
     self.questionDifficulty.text = [NSString stringWithFormat:@"Уровень сложности: %d", self.questionsType];
-
 }
 
+
+#pragma mark - Other methods
+
 - (void)shuffle:(NSMutableArray *)array {
-    
     for (NSUInteger i = [array count]; i > 1; i--) {
-        [array exchangeObjectAtIndex: i - 1 withObjectAtIndex:i];
+        [array exchangeObjectAtIndex: i - 1 withObjectAtIndex:arc4random_uniform((u_int32_t)i)];
     }
-    
+}
+
+- (void)fiftyFiftyAnswers {
+    NSPredicate *falseAnswerPredicate = [NSPredicate predicateWithFormat:@"SELF != %@",self.trueAnswer];
+    NSArray *falseAnswers = [self.questionAndAnswers.answers filteredArrayUsingPredicate:falseAnswerPredicate];
+    NSString *randomAnswer = [falseAnswers objectAtIndex:arc4random_uniform((u_int32_t)[falseAnswers count])];
+    self.questionAndAnswers.answers = [NSMutableArray arrayWithObjects:self.trueAnswer,randomAnswer,nil];
+    //добавить shuffle
+    [self.tableView reloadData];
 }
 
 @end
